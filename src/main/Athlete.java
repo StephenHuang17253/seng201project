93a580bf19aece37a43f091304ead4e5c8d9acf3
StephenHuang17253 <17253@burnside.school.nz>
2023-05-06 18:26:47 +1200

    Added some javadoc for Athlete class

--- src/main/Athlete.java
+++ src/main/Athlete.java
@@ -1,19 +1,52 @@
 package main;
 
+
+/**
+ * The Athlete class
+ * handles the athletes in our game.
+ */
 public class Athlete implements Purchasable{
 
+
+	/**
+	 * The Athlete's name.
+	 */	
 	private String name;
+	/**
+	 * The Athelete's health stat.
+	 */		
 	private int health;
+	/**
+	 * The Athelete's stamina stat.
+	 */			
 	private int stamina;
+	/**
+	 * The Athelete's offence stat.
+	 */			
 	private int offence;
+	/**
+	 * The Athelete's defence stat.
+	 */		
 	private int defence;
+	/**
+	 * The Athelete's contract price.
+	 */		
 	private int contractPrice;
+	/**
+	 * The Athelete's sell back price.
+	 */		
 	private int sellbackPrice;
+	/**
+	 * The Athelete's description.
+	 */	
 	private String description;
 	
 	
+
+	/**
+	 * Constructor for Athlete
+	 */		
 	public Athlete(String name, int health, int stamina, int offence, int defence) {
-		// TODO Auto-generated constructor stub
 		this.name = name;
 		this.health = health;
 		this.stamina = stamina;
@@ -21,79 +54,119 @@ public class Athlete implements Purchasable{
 		this.defence = defence;
 	}
 
+
+	/**
+	 * Purchase method for Athlete.
+	 * Doesn't do anything right now.
+	 */		
 	public void purchase(int contractPrice, int sellbackPrice, String description) {
 		this.setContractPrice(contractPrice);
 		this.setSellbackPrice(sellbackPrice);
 		this.setDescription(description);
 		
 	}	
-	
+
+	/**
+	 * Simple getter for Athlete's name.
+	 */		
 	public String getName() {
 		return name;
 	}
-
+	/**
+	 * Simple setter for Athlete's name.
+	 */		
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	/**
+	 * Simple getter for Athlete's health.
+	 */		
 	public int getHealth() {
 		return health;
 	}
-
+	/**
+	 * Simple setter for Athlete's health.
+	 */		
 	public void setHealth(int health) {
 		this.health = health;
 	}
-	
+	/**
+	 * Simple getter for Athlete's stamina.
+	 */			
 	public int getStamina() {
 		return stamina;
 	}
-
+	/**
+	 * Simple setter for Athlete's stamina.
+	 */		
 	public void setStamina(int stamina) {
 		this.stamina = stamina;
 	}
-	
+	/**
+	 * Simple getter for Athlete's offence.
+	 */			
 	public int getOffence() {
 		return offence;
 	}
-
+	/**
+	 * Simple setter for Athlete's offence.
+	 */		
 	public void setOffence(int offence) {
 		this.offence = offence;
 	}
-
+	/**
+	 * Simple getter for Athlete's defence.
+	 */	
 	public int getDefence() {
 		return defence;
 	}
-
+	/**
+	 * Simple setter for Athlete's defence.
+	 */	
 	public void setDefence(int defence) {
 		this.defence = defence;
 	}
-
+	/**
+	 * Athlete's toString, handles printing Athletes.
+	 */	
 	public String toString() {
 		return "Athlete Stats" + "\nName: " + name + "\nHealth: " + health + "/100" + "\nStamina: " + stamina + "/10"
 				+ "\nOffence: " + offence + "/10" + "\nDefence: " + defence + "/10";
 				
 	}
-	
+	/**
+	 * Simple getter for Athlete's contract price.
+	 */		
 	public int getContractPrice() {
 		return contractPrice;
 	}
-
+	/**
+	 * Simple setter for Athlete's contract price.
+	 */		
 	public void setContractPrice(int contractPrice) {
 		this.contractPrice = contractPrice;
 	}
-
+	/**
+	 * Simple getter for Athlete's sell price.
+	 */		
 	public int getSellbackPrice() {
 		return sellbackPrice;
 	}
-
+	/**
+	 * Simple setter for Athlete's contract price.
+	 */		
 	public void setSellbackPrice(int sellbackPrice) {
 		this.sellbackPrice = sellbackPrice;
 	}
-
+	/**
+	 * Simple getter for Athlete's description.
+	 */		
 	public String getDescription() {
 		return description;
 	}
-
+	/**
+	 * Simple setter for Athlete's contract price.
+	 */		
 	public void setDescription(String description) {
 		this.description = description;
 	}
